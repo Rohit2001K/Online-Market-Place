1bd1b6928453d770039debddfd9a99ce680ab848
Rohit2001K <rohitkumavatgame@gmail.com>
2024-09-18 21:20:27 +0530

    Product Model Update/ Read in HTML Page
    Add some importand fields.
    Add user drop down menu.
    Add Market page.
    Reading Product model

--- store/market/views.py
+++ store/market/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
 
 
 #market home page
 
 def market_page(request):
-    return render(request,'market/market_page.html')
+    products=Product.objects.all()
+    content={
+        'products':products
+    }
+    return render(request,'market/market_page.html',content)
